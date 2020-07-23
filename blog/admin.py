81d853f34ea646ca2b1ba62f167c9a7d7c92467f
Elder Loiola Lima <g41jkx6a2u4s@gmail.com>
2020-07-23 17:31:06 -0300

    atualizando

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import Post
 # Register your models here.
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'autor', 'publicado', 'status')
+    list_filter = ('status', 'criado','publicado','autor')
+    raw_id_fields = ('autor',)
+    search_fields = ('title', 'conteudo',)
+    prepopulated_fields = {'slug':('title',)}
+    date_hierarchy = 'publicado'
